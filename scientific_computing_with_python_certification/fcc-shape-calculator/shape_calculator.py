8020424d4b5547b517f7b3918d95e5a2b97cb9ed
EakzIT <pilpaff@gmail.com>
2020-08-12 08:50:49 +0300

    Finished the answer

--- scientific_computing_with_python_certification/fcc-shape-calculator/shape_calculator.py
+++ scientific_computing_with_python_certification/fcc-shape-calculator/shape_calculator.py
@@ -1,6 +1,55 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
+
+    def set_width(self, value):
+        self.width = value
+
+    def set_height(self, value):
+        self.height = value
+
+    def get_area(self):
+        return self.width*self.height
+
+    def get_perimeter(self):
+        return 2*self.width+2*self.height
+
+    def get_diagonal(self):
+        return (self.width**2+self.height**2)**.5
+
+    def get_picture(self):
+        if (self.width > 50 or self.height > 50):
+            return "Too big for picture."
+        return "\n".join(["*"*self.width for _ in range(self.height)])+'\n'
+
+    def get_amount_inside(self, other):
+        return int(self.width/other.width*self.height/other.height)
 
 
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+        self.side = side
+
+    def __str__(self):
+        return f"Square(side={self.side})"
+
+    def set_side(self, value):
+        self.side = value
+        self.height = value
+        self.width = value
+
+    def set_width(self, value):
+        self.set_side(value)
+
+    def set_height(self, value):
+        self.set_side(value)
 
 
-class Square:
+bonito = Rectangle(15, 15)
+
+print(bonito.get_picture())
